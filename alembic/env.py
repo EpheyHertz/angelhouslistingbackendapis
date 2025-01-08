b7e38f6d3c2a71addc0b265906259daf1d26c7ed
EpheyHertz <mwain579@gmail.com>
2025-01-08 23:02:25 +0300

    Changed the database to neon

--- alembic/env.py
+++ alembic/env.py
@@ -5,12 +5,12 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.models import Base
-from app.config import DATABASE_ALEMBIC_URL
+from app.config import DATABASE_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url",DATABASE_ALEMBIC_URL)
+config.set_main_option("sqlalchemy.url",DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
